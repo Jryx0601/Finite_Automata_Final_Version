a7992c6c77ac1898ea168ce2636e696d33f904d9
Jryx0601 <samuraikeneki@gmail.com>
2025-05-20 19:50:27 +0800

    final

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from graphviz import Digraph
 from dfa_checker import dfa_checker
 import time
 from PIL import Image
+from streamlit_extras.switch_page_button import switch_page
 import pathlib
 st.set_page_config(layout="wide",initial_sidebar_state='collapsed')
 
@@ -51,7 +52,7 @@ with tab1:
     with col4:
         modal = st.button('Push Down Automata Binary')
         if modal:
-            st.switch_page('pages/PDA1.py')
+            switch_page('PDA1')
 #------------------------------------------------------------------------------------------------------
     dot = Digraph()
     place_holder = st.empty()
