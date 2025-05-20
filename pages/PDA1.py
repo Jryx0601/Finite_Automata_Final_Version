69e4d436124c119da61710122b32a929c7a1141e
Jryx0601 <samuraikeneki@gmail.com>
2025-05-20 20:30:49 +0800

    FINAL_AUTOMATA_COMPILER

--- pages/PDA1.py
+++ pages/PDA1.py
@@ -3,7 +3,8 @@ from PIL import Image
 
 st.header('Push Down Automata Regex 1')
 img = Image.open('PDA.drawio.png')
-st.image(img)
-
+# st.image(img)
+col1,col2,col3 = st.columns([1,10,1])
+col2.image(img)
 if st.button('Back to Homepage'):
     st.switch_page('main.py')
--- pages/PDA2.py
+++ pages/PDA2.py
@@ -3,7 +3,10 @@ from PIL import Image
 
 st.header('Push Down Automata Regex 2')
 img = Image.open('PDA2_Regex.png')
-st.image(img)
+# st.image(img)
+
+col1,col2,col3 = st.columns([1,10,1])
+col2.image(img)
 
 if st.button('Back to Homepage'):
     st.switch_page('main.py')
